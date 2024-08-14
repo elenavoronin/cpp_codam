ff6e79d9f94ff0384b9983c73c6acfbb64b59a56
Elena Voronin <evoronin@f1r7s1.codam.nl>
2024-08-14 13:41:11 +0200

    created ex02

---
+++ cpp02/Fixed.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+
+class Fixed {
+    private:
+
+
+    public:
+        Fixed();
+        Fixed(const Fixed& copy);
+        ~Fixed();
+        Fixed& operator=(const Fixed&);
+};
---
+++ cpp02/main.cpp
@@ -0,0 +1,32 @@
+#include "Fixed.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+
+int main() {
+    Phonebook phonebook;
+	phonebook.setcount(0);
+	phonebook.setoldest(0);
+    while (1)
+    {
+        std::cout << "ADD, SEARCH or EXIT" << std::endl;
+        std::string command;
+        std::getline(std::cin, command);
+        if (command == "ADD") {
+            Contact contact;
+            contact.setContact();
+            phonebook.add(contact);
+		}
+        else if (command == "SEARCH") {
+            phonebook.search();
+        }
+        else if (command == "EXIT") {
+            break ;
+            }
+        else {
+            std::cout << "Wrong command" << std::endl;
+        }
+    }
+    return 0;
+}
