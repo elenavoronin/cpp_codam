ed1eb2221468011a978ef6fc6a1a2478c319e553
elenavoronin <elnvoronin@gmail.com>
2025-04-03 13:38:04 +0200

    finished cpp06/ex00

--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -1,4 +1,4 @@
-#include "../include/ScalarConverter.hpp"
+#include "ScalarConverter.hpp"
 
 
 ScalarConverter::ScalarConverter() {
@@ -42,7 +42,7 @@ bool isFloat(std::string str) {
     if (str[i] == '+' || str[i] == '-')
         i++;
     std::string string = str.substr(i);
-    if (string.compare("-inff") || string.compare("+inff") || string.compare("nanf"))
+    if (string.compare("inff") || string.compare("nanf"))
         return true;
     for (char c : str) {
         if (c != '.' && c != 'f' && !isdigit(c))
@@ -56,7 +56,7 @@ bool isDouble(std::string str) {
     if (str[i] == '+' || str[i] == '-')
         i++;
     std::string string = str.substr(i);
-    if (string.compare("-inf") || string.compare("+inf") || string.compare("nan"))
+    if (string.compare("inf") || string.compare("nan"))
         return true;
     for (char c : str) {
         if (c != '.' && !isdigit(c))
@@ -89,9 +89,7 @@ void printChar(const std::string str) {
 
 void printInt(const std::string str) {
     int result = std::stoi(str);
-    std::cout << result << std::endl;
-
-    if (!checkLimit(&result) && result < INT_MAX && result > INT_MIN)
+    if (!checkLimit(&result) && result < 0 && result > 127)
         std::cout << "char: " << static_cast<char>(result) << std::endl;
     else
         std::cout << "char: not convertable" << std::endl;
@@ -102,28 +100,32 @@ void printInt(const std::string str) {
 
 void printFloat(const std::string str) {
     float result = std::stof(str);
-
-    if (!checkLimit(&result) && !str.compare("nanf") && !str.compare("+inff") && !str.compare("-inff"))
+    if (checkLimit(&result) && result > 0 && result < 127 && str.compare("nanf") && str.compare("+inff") && str.compare("-inff"))
         std::cout << "char: " << static_cast<char>(result) << std::endl;
     else
-        std::cout << "char: not convertable" << std::endl;
+        std::cout << "char: not convertible" << std::endl;
 
     if (static_cast<int>(result) < INT_MAX && static_cast<int>(result) > INT_MIN)
         std::cout << "int: " <<  static_cast<int>(result) << std::endl;
     else
         std::cout << "int: " <<  "not convertible" << std::endl;
-    std::cout << "float: " << result << std::endl;
-    if (str.compare("nanf") || str.compare("-inff") || str.compare("+inff"))
+	if (str == "nanf" || str == "-inff" || str == "+inff")
+	{
+		std::cout << "float: " << str << std::endl;
         std::cout << "double: " << str.substr(0, str.size() - 1) << std::endl;
+	}
     else
-        std:: cout << "double: " << static_cast<double>(result) << std::endl;
+	{
+		std::cout << "float: " << str << std::endl;
+        std:: cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double>(result) << std::endl;
+	}
 }
 
 
 void printDouble(const std::string str) {
     double result = std::stod(str);
 
-    if (!checkLimit(&result) && !str.compare("nan") && !str.compare("+inf") && !str.compare("-inf"))
+    if (!checkLimit(&result) && str.compare("nan") && str.compare("+inf") && str.compare("-inf"))
         std::cout << "char: " << static_cast<char>('0' + result) << std::endl;
     else
         std::cout << "char: not convertable" << std::endl;
@@ -131,11 +133,16 @@ void printDouble(const std::string str) {
         std::cout << "int: " <<  static_cast<int>(result) << std::endl;
     else
         std::cout << "int: " <<  "not convertible" << std::endl;
-    if (str.compare("nan") || str.compare("-inf") || str.compare("+inf"))
-        std::cout << "float: " << str + "f" << std::endl;
-    else
-        std::cout << "float: " << static_cast<float>(result) << std::endl;
-    std:: cout << "double: " << result << std::endl;
+    if (str == "nan" || str== "-inf" || str == "+inf")
+	{
+        std::cout << "float: " << str << "f" << std::endl;
+		std:: cout << "double: " << str << std::endl;
+	}
+	else
+	{
+		std::cout << "float: " << static_cast<float>(result) << std::endl;
+		std:: cout << "double: " << result << std::endl;
+	}
 }
 
 
